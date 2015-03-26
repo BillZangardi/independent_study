6872301dfa520f4136b756ef6ea1cc5e9385a746
billzangardi <billz@rappiddevelopment.com>
2015-03-26 12:28:30 -0400

    Remove absolute path

--- CreateNewBackup.php
+++ CreateNewBackup.php
@@ -1,3 +1,3 @@
 <?php
-	echo shell_exec("sdk/platform-tools/adb backup -all -f /Users/zangardiw/sites/Independent_Study/data/backups/compressed/$(date \"+%Y.%m.%d-%H.%M.%S\").ab 2>&1");
+	echo shell_exec("sdk/platform-tools/adb backup -all -f data/backups/compressed/$(date \"+%Y.%m.%d-%H.%M.%S\").ab 2>&1");
 ?>
